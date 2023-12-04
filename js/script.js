a320442fc363b9d9dad046403f38a4150e48eee9
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-12-04 14:36:25 +0100

    creazione array di oggetti

--- js/script.js
+++ js/script.js
@@ -4,7 +4,60 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-        
+        listaCarteBannate : [
+            {
+                text : 'BirthingPod',
+                done : true
+            },
+
+            {
+                text : 'Mox Opal',
+                done : true
+            },
+
+            {
+                text : 'Grief',
+                done : false
+            },
+
+            {
+                text : 'Splinter Twin',
+                done : true
+            },
+
+            {
+                text : 'Fury',
+                done : false
+            },
+
+            {
+                text : 'Preordain',
+                done : false
+            },
+
+            {
+                text : 'Yawgmoth, Thran Physician',
+                done : false
+            },
+
+            {
+                text : 'Oko, Thief of Crowns',
+                done : true
+            },
+
+            {
+                text : 'Uro, Titan of Nature Wrath',
+                done : true
+            },
+
+            {
+                text : 'Urza, Lord High Artificer',
+                done : false
+            },
+        ]
     }
+    }, 
+    methods: {
+        
     }
 }).mount('#app')
